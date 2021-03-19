b5feba217abc4225ba3d617649e27b54dab6146a
Erick Gerardo Calderon Reyes <A01351361@itesm.mx>
2021-03-19 09:55:29 -0600

    Pacman pep8

--- pacman.py
+++ pacman.py
@@ -1,6 +1,6 @@
 from random import choice
 from turtle import bgcolor, clear, up, goto, dot, update, \
-    ontimer, setup, hideturtle, tracer, listen, onkey, done
+    ontimer, setup, hideturtle, tracer, listen, onkey, done, Turtle
 from freegames import floor, vector
 
 state = {'score': 0}
